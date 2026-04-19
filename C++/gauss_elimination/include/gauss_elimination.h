ce5935c6d7628f59f2987095581e6102f04d1946
Hakeem-Taha-06 <hakeem.taha06@eng-st.cu.edu.eg>
2026-04-19 12:48:01 +0200

    add placeholder for C++ gauss elimination

---
+++ C++/gauss_elimination/include/gauss_elimination.h
@@ -0,0 +1,8 @@
+﻿// gauss_elimination.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
---
+++ C++/gauss_elimination/src/gauss_elimination.cpp
@@ -0,0 +1,12 @@
+﻿// gauss_elimination.cpp : Defines the entry point for the application.
+//
+
+#include "gauss_elimination.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
